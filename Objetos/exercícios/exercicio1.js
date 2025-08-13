21f82cb9ba57c340b6e73ab410d39105d89dee0f
Miguel Henrique <miguelhenriqueisaac@gmail.com>
2025-08-12 22:09:49 -0300

    exe2

--- Objetos/exercícios/exercicio1.js
+++ Objetos/exercícios/exercicio1.js
@@ -1,6 +1,41 @@
-let biblio = []
+let biblio = [], maisPag = 0, mediaPag = 0, somaPag = 0
 
 for(let i = 0; i < 5;i++){
     let titulo = prompt(`informe o titulo do ${i+i}° livro`)
-    let autor = prompt(``)
+    let autor = prompt(`\nInforme o autor do ${i+1}° livro`)
+    let publicacao = prompt(`\ninforme o ano de publicação do ${i+1}° livro`)
+    let quantiPag = prompt(`\nInforme a quantidade de páginas do ${i+1}° livro`)
+    somaPag =+ biblio[i].quantiPag;
+
+    let livros ={
+        titulo:titulo,
+        autor:autor,
+        publicacao:publicacao,
+        quantiPag:quantiPag,
+
+    }
+    biblio.push(livros)
 }
+
+
+
+for(let i = 0; i < 5;i++){
+    if (biblio[i].quantiPag > 300){
+        console.log(`\nLivros com mais de 300 paginas : \n`,biblio[i])
+    }  
+
+    mediaPag =+ biblio[i].quantiPag;
+
+    maisPag = biblio[0]
+
+    if(maisPag < biblio[i].quantiPag){
+        maisPag = biblio[i];
+    }
+
+}
+
+// exibir
+console.log(`\na média de paginas é de: `,mediaPag / 5)
+console.log(`\no livro com mais paginas é: `,maisPag)
+
+
---
+++ Objetos/exercícios/exercicio2.js
@@ -0,0 +1,33 @@
+let funcionarios = [], maiorS=0, mediasalario=0 , qntGerente = 0
+
+for(let i = 0; i <6; i++){
+    let funcionario = {
+        nome:nome = prompt(`insira o nome do ${i+1}° funcionario`),
+        cargo:cargo = prompt(`\nInforme o cargo do funcionario ${i+1}`).toLowerCase(),
+        salario:salario = Number(prompt(`\ninforme o salário do funcionário ${i+1}`)),
+        tempoServico:tempoServico = prompt(`\ninforme o tempo de serviço do funcionario ${i+1} (em anos)`)
+    }
+    funcionarios.push(funcionario)
+
+}
+
+for(let i = 0; i <6; i++ ){
+    if(funcionarios[i].salario > 5000 && funcionarios[i].tempoServico > 5){
+        console.log(`\nFuncionarios com mais de 5 anos e recebendo mais de R$5.000:  \n`,funcionarios[i])
+    }
+
+    maiorS = funcionarios[0]
+    if(funcionarios[i].salario > maiorS.salario){
+        maiorS = funcionarios[i]
+    }
+
+    mediasalario += funcionarios[i].salario
+
+    if(cargo === "gerente"){
+        qntGerente++
+    }
+}
+
+console.log (`\nO maior salario é de: ${maiorS.nome}`)
+console.log(`\nmedia salarial de: `, mediasalario / 6)
+console.log(`\na empresa tem ${qntGerente} gerentes`)
